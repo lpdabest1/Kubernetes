3343800216cc34e1c6d8435017780903032635c8
lpdabest1 <lorenzo.pesina@utexas.edu>
2022-12-09 14:10:16 -0600

    edits

--- Scripts/eks_anywhere/eks_anywhere_create_local_cluster.sh
+++ Scripts/eks_anywhere/eks_anywhere_create_local_cluster.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Install Kubernetes Components needed
+# Kubelet, Kubectl, Kubeadm
+
+# Update the apt package index and install packages needed to use the Kubernetes apt repo:
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Download the Google Cloud public signing key
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+# Add the Kubernetes apt repo:
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install Kubelet, Kubectl, Kubeadm
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
 # Create a local cluster
 
 # Generate a cluster config
